562cab20b230cba14540e81ebf7fbeb49ad88eca
Suhayl Amla <srextravis@Suhayls-Mac.local>
2023-11-02 13:53:47 +0000

    Functional board which uses Usestate to update the value

--- brain/src/App.js
+++ brain/src/App.js
@@ -1,28 +1,29 @@
-
+import React from 'react'
 import './App.css';
 import Square from './Square.js'
+//import { useState } from 'react';
 
-const S1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 function App() {
   return (
     <div>
     <div className="board-row">
-    <Square S1 = {S1[0]}></Square>
-    <Square S1 = {S1[1]}></Square>
-    <Square S1 = {S1[2]}></Square>
+    <Square ></Square>
+    <Square ></Square>
+    <Square ></Square>
     </div>
 
     <div className="board-row">
-    <Square S1 = {S1[3]}></Square>
-    <Square S1 = {S1[4]}></Square>
-    <Square S1 = {S1[5]}></Square>
+    <Square ></Square>
+    <Square ></Square>
+    <Square ></Square>
     </div>
 
+
     <div className="board-row">
-    <Square S1 = {S1[6]}></Square>
-    <Square S1 = {S1[7]}></Square>
-    <Square S1 = {S1[8]}></Square>
+    <Square ></Square>
+    <Square ></Square>
+    <Square ></Square>
     </div>
     </div>
   );
--- brain/src/Square.js
+++ brain/src/Square.js
@@ -1,7 +1,21 @@
-const Square = ({S1}) => {
-    return (
-        <div className="square"> {S1} </div>
-      );
-}
- 
-export default Square;
+import React from "react"
+import { useState } from "react" //Ensure to import anything you need
+
+
+const Square = () => {
+    
+    const [value, setvalue] = useState(null) // this is the usage of useState
+
+    const Onclicks = () => { //This is a function for onclicks which uses the setvalue function to assign the value const a new value
+    console.log('HELLO')
+    setvalue('X')
+
+    } // make sure you arent defining variavles and creating functions within the return section. The return section is simply for the JSX you want to return 
+    // alongside with the use of curly braces to reference any javascript - eg variables / functions / 
+        return (
+            <div className="square" onClick={Onclicks}>{value}</div> // onclick= is an event handler so on the click {onclick} the curly braces calls the JS expression and runs the function
+        );
+    }
+    
+export default Square; //must export square as we are rendering it on our main App.js
+//Square acts as a component which is reuseable
