7ebafc0a1bd0b8bd655c389ec92270634fd064d2
Suhayl Amla <srextravis@Suhayls-Mac.local>
2023-11-02 15:35:33 +0000

    halfway

--- brain/src/Square.js
+++ brain/src/Square.js
@@ -1,19 +1,26 @@
 import React from "react"
-import { useState } from "react" //Ensure to import anything you need
+//import { useState } from "react" //Ensure to import anything you need
 
 
-const Square = () => {
+const Square = ({value, onSquareClick}) => {
+   
     
-    const [value, setvalue] = useState(null) // this is the usage of useState
+   // const [value, setvalue] = useState(null) // this is the usage of useState
 
-    const Onclicks = () => { //This is a function for onclicks which uses the setvalue function to assign the value const a new value
-    console.log('HELLO')
-    setvalue('X')
-
-    } // make sure you arent defining variavles and creating functions within the return section. The return section is simply for the JSX you want to return 
+   // const Onclicks = () => { //This is a function for onclicks which uses the setvalue function to assign the value const a new value
+   // console.log('HELLO')
+   // setValue('X')
+   // } 
+    
+    // make sure you arent defining variavles and creating functions within the return section. The return section is simply for the JSX you want to return 
     // alongside with the use of curly braces to reference any javascript - eg variables / functions / 
         return (
-            <div className="square" onClick={Onclicks}>{value}</div> // onclick= is an event handler so on the click {onclick} the curly braces calls the JS expression and runs the function
+            <div>
+            
+            <button className="square" onClick={onSquareClick}>
+                {value}
+                </button>
+            </div> // onclick= is an event handler so on the click {onclick} the curly braces calls the JS expression and runs the function
         );
     }
     
